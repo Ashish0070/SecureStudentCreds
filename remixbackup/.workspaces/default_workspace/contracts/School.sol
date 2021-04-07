59bca0d536c0a6afa424ebd1538e39bdff7e8860
Ashish0070 <46740232+Ashish0070@users.noreply.github.com>
2021-04-07 16:11:32 +0530

    Update School.sol

--- remixbackup/.workspaces/default_workspace/contracts/School.sol
+++ remixbackup/.workspaces/default_workspace/contracts/School.sol
@@ -12,11 +12,11 @@ contract School is TestUser {
     address[] public schoolStudents;
     address[] public studentToSchool;
     mapping(address=>uint) public studentCount;
-    uint public totalStudents;
+    uint public totalStudentsEnrolled;
     FileContract public fc;
     Student public s;
-    address public FileContractAddress = 0x9841B68a56c7E70EAa8675AAE8629600dfBEBC96;  //set it later
-    address public StudentContractAddress = 0x0bFCfA4092a19E9B78Eb32Eb4011B68b51Fbdb8e;  //set it later
+    address public FileContractAddress = 0x2B382c265570168FE9F2a227C8B9B0B8c4E5FDA8;  //set it later
+    address public StudentContractAddress = 0x4c9dE3d4a770a32AD8d8989993ef190d129FeD9b;  //set it later
     
     
     constructor() public {
@@ -25,10 +25,10 @@ contract School is TestUser {
     }
 
     
-    //check is student is present in school
-    function hasStudent (address _student, address _school) public view returns(bool) {
-        // require(s.hasUser(_student));
-        for(uint i=0;i<totalStudents;i++){
+    //check if student is present in school
+    function hasStudentInSchool (address _student, address _school) public view returns(bool) {
+        require(s.hasStudentUser(_student));
+        for(uint i=0;i<totalStudentsEnrolled;i++){
             if(schoolStudents[i]==_student){
                 if(studentToSchool[i]==_school){
                     return true;
@@ -41,17 +41,17 @@ contract School is TestUser {
     
     //add  student to school - only school calls this function to enrol student to itself
     function addStudent (address _student) public {
-        // require(!hasStudent(_student, msg.sender));
+        require(!hasStudentInSchool(_student, msg.sender));
         schoolStudents.push(_student);
         studentToSchool.push(msg.sender);
         studentCount[msg.sender]++;
-        totalStudents++;
+        totalStudentsEnrolled++;
     }
     
     
     //return student details
     function getStudent (address _student) public view returns(user memory) {
-        return s.getUser(_student);
+        return getStudentUser(_student);
     }
     
     
@@ -63,44 +63,29 @@ contract School is TestUser {
     
     //returns list of students if a school - called by company (maybe let school call this to produce a list of students to display on dashboard)
     function getStudents (address _school) public view returns(address[] memory) {
-        address[] memory students = new address[](studentsCount(_school));
+        address[] memory studentsEnrolled = new address[](studentsCount(_school));
         uint id=0;
-        for(uint i=0;i<totalStudents;i++){
+        for(uint i=0;i<totalStudentsEnrolled;i++){
             if(studentToSchool[i]==_school){
-                students[id] = schoolStudents[i];
+                studentsEnrolled[id] = schoolStudents[i];
                 id++;
             }
         }
         
-        return students;
+        return studentsEnrolled;
     }
     
     
     //lets school upload a file(with ipfs hash) for a student 
     function uploadFileSchool (string memory ipfsHash, string memory title, string memory description, address _student) public {
-        this.hasStudent(_student, msg.sender);
+        require(hasStudentInSchool(_student, msg.sender));
         fc.uploadFile(ipfsHash, title, description, _student);
     }
     
     
     //returns the list of all schools - called by company
     function getSchools() public view returns (address[] memory) {
-        return addressOfUser;
+        return addressOfSchools;
     }
-    
-    
-    //how many files  exist for a student (maybe not needed)
-    // function getFileCountSchool (address _student) public view returns(uint){
-    //     require(s.hasUser(_student)); //if student exists
-    //     require(msg.sender == studentToSchool[_student]);  //if student is a part of this school
-    //     return s.getFileCount(_student);
-    // }
-    
-    //returns all files of a student (maybe not needed)
-    // function getFileSchool (address _student) public view returns(FileContract.File[] memory) {
-    //     require(s.hasUser(_student)); //if student exists
-    //     require(msg.sender == studentToSchool[_student]);  //if student is a part of this school
-    //     return s.getFile(_student);
-    // }
     
 }
