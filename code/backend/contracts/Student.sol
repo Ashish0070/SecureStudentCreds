4f9ec60fa4fc94a6b25ee97dd3c6f55136f9eec2
Ashish0070 <46740232+Ashish0070@users.noreply.github.com>
2021-04-28 17:38:54 +0530

    Update Student.sol

--- code/backend/contracts/Student.sol
+++ code/backend/contracts/Student.sol
@@ -3,25 +3,26 @@
 pragma solidity ^0.6.0;
 pragma experimental ABIEncoderV2;
 import './FileContract.sol';
+import  './TestUser.sol';
 
-contract Student {
+contract Student is TestUser{
     
-    FileContract fc;  //all files owned by a student
-    address FileContractAddress;  //set it later(deployed one)
+    FileContract public fc;  //all files owned by a student
+    address public FileContractAddress = 0x2B382c265570168FE9F2a227C8B9B0B8c4E5FDA8;  //set it later(deployed one)
     
     constructor() public {
         //create an instance of File Contract using the deployed File Contract's address
         fc = FileContract(FileContractAddress);
     }
     
+    // return no of files owned by this student
     function getFileCount() public view returns (uint) {
-        // return no of files owned by this student
-        return fc.getFileCount();
+        return fc.getFileCount(msg.sender);
     }
     
-    function getFile() public view returns (FileContract.File memory) {
-        // return all files owned by this student
-        return fc.getFile();
+    // return all files owned by this student
+    function getFile() public view returns (FileContract.File[] memory) {
+        return fc.getFile(msg.sender);
     }
     
 }
